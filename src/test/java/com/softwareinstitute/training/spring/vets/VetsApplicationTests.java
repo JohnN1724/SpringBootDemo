c906cd0698214960497b2434066230666027f553
Johnn <john.nuttall17@gmail.com>
2022-08-11 09:47:53 +0100

    Updated POM with jacoco and junit and added unit tests

--- src/test/java/com/softwareinstitute/training/spring/vets/VetsApplicationTests.java
+++ src/test/java/com/softwareinstitute/training/spring/vets/VetsApplicationTests.java
@@ -3,11 +3,45 @@ package com.softwareinstitute.training.spring.vets;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class VetsApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void get_cat_name() {
+
+		Cat cat = new Cat();
+
+		assertEquals("Ginger", cat.getName(), cat.getName());
+	}
+
+	@Test
+	public void get_bat_flying(){
+
+		Bat bat = new Bat();
+
+		assertEquals("I'm flying!!", bat.flying());
+	}
+
+	@Test
+	public void get_bird_layingEgg() {
+
+		Penguin penguin = new Penguin();
+
+		assertEquals("I'm laying an egg!!", penguin.getLayEgg());
+	}
+
+	@Test
+	public void get_penguin_swimming() {
+
+		Penguin penguin = new Penguin();
+
+		assertEquals("I'm swimming!!", penguin.swim());
 	}
 
 }
