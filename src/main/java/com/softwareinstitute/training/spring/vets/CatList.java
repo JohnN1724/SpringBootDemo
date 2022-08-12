a7f7e84a747bdba245e1f51f632c28fa094fceee
Johnn <john.nuttall17@gmail.com>
2022-08-12 11:55:52 +0100

    Changed array list name to match VetsApplication

--- src/main/java/com/softwareinstitute/training/spring/vets/CatList.java
+++ src/main/java/com/softwareinstitute/training/spring/vets/CatList.java
@@ -1,33 +1,35 @@
 package com.softwareinstitute.training.spring.vets;
 
 import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class CatList {
 
-    private List<Cat> animalList = new ArrayList();
+    private List<Cat> myCatList = new ArrayList();
 
 
-    public void animalList(){
-        this.animalList.add(new Cat("Mammal", "Cat", "Tabby", "Fluffy"));
+    public void myCatList(){
+        this.myCatList.add(new Cat("Mammal", "Cat", "Tabby", "Fluffy"));
     }
 
-    public List<Cat> getAnimalList(){
-        return this.animalList;
+    public List<Cat> getMyCatList(){
+        return this.myCatList;
     }
 
     public String toString(){
-        String json = new Gson().toJson(animalList);
+        String json = new Gson().toJson(myCatList);
         return  json;
     }
 
     public void addCatList(Cat c){
-        this.animalList.add(c);
+        this.myCatList.add(c);
     }
 
     public void changeCatList(int id,Cat c){
-        this.animalList.set(id,c);
+        this.myCatList.set(id,c);
     }
 }
