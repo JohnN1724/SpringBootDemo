a70f947167fe71d173007f029bd290ad178ec553
Johnn <john.nuttall17@gmail.com>
2022-08-12 13:58:17 +0100

    Created second application to send information between two microservices

---
+++ src/test/java/com/softwareinstitute/training/spring/vets/CatsApplication.java
@@ -0,0 +1,41 @@
+package com.softwareinstitute.training.spring.vets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class CatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatsApplication.class, args);
+    }
+
+    public static CatList myCatList = new CatList();
+
+
+    @CrossOrigin("http://localhost:8080")
+    @GetMapping("/customRoute")
+    @ResponseBody
+    public String myResponse(){
+        return myCatList.toString();
+    }
+
+    @PutMapping("/change/{id}")
+    @ResponseBody
+    public void change(@PathVariable int id, @RequestParam String Mammal, @RequestParam String animalType,
+                       @RequestParam String catType, @RequestParam String name) {
+        myCatList.changeCatList(id,new Cat());
+
+    }
+
+    @PutMapping("/add")
+    @ResponseBody
+    public void change(@RequestParam String Mammal, @RequestParam String animalType,
+                       @RequestParam String catType, @RequestParam String name) {
+        myCatList.addCatList(new Cat(Mammal, animalType, catType, name));
+
+    }
+
+}
+
